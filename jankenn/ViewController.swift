7ba5d7d92b61146afc9fa54dc64f2c9b9aec7b27
ginzo <ginzo@ginzonomacbook-puro.local>
2018-07-30 10:04:25 +0900

    Initial Commit

---
+++ jankenn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jankenn
+//
+//  Created by ginzo on 2018/07/30.
+//  Copyright © 2018年 ginzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
