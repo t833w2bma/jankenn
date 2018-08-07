ecd2bef8c0683a06f006d18a150e654075cbd480
t833w2bma <41927549+t833w2bma@users.noreply.github.com>
2018-08-07 21:13:50 +0900

    Update ViewController.swift

--- jankenn/ViewController.swift
+++ jankenn/ViewController.swift
@@ -7,10 +7,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cpulb: UILabel!
     @IBOutlet weak var keka: UILabel!
-    @IBOutlet weak var yourlb: UILabel!
-    let tearr=["✊","✌️","🖐"]   //じゃんけんの配列
-    
+   
+  
     @IBAction func tapGu(_ sender: Any) {
+        let tearr=["✊","✌️","🖐"]   //じゃんけんの配列
         let teid = Int(arc4random_uniform(3))
         print(teid)
         cpulb.text = tearr[teid]  // CPUのラベルに手を表示
@@ -25,50 +25,17 @@ class ViewController: UIViewController {
         
     }
     
-    
-    @IBAction func tapChoki(_ sender: Any) {
-        let teid = Int(arc4random_uniform(3))
-        print(teid)
-        cpulb.text = tearr[teid]  // CPUのラベルに手を表示
-         yourlb.text = "✌️"
-        if teid == 2{
-            keka.text="あなたの勝ち"
-        }else if teid == 0{
-            keka.text = "あなたの負け"
-        }else {
-            keka.text = "あいこ"
-        }
-    }
-    
    
-    
-    @IBAction func tapPar(_ sender: Any) {
-        let teid = Int(arc4random_uniform(3))
-        print(teid)
-        cpulb.text = tearr[teid]  // CPUのラベルに手を表示
-         yourlb.text = "🖐"
-        if teid == 0{
-            keka.text="あなたの勝ち"
-        }else if teid == 1{
-            keka.text = "あなたの負け"
-        }else {
-            keka.text = "あいこ"
-        }
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 180度反転
-        cpulb.transform = CGAffineTransform(rotationAngle: 3.1415);
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+  
     
 }
 
